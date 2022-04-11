1c0b0121d78f06b32f755c47862eae2c62673688
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-04-11 20:39:28 +0200

    chore: update script

--- scripts/copyAddonToWOW.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  rm -rf /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/BBUI/*
-  cp ../BBUI.toc /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/BBUI/BBUI.toc
-  cp ../main.lua /Applications/World\ of\ Warcraft/_retail_/Interface/AddOns/BBUI/main.lua
-else
-  echo 'Unsupported OS'
-fi
